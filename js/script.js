63f24538bf741505f5189dabce08570d24b8c05f
chels <cstauber77@gmail.com>
2016-02-23 17:07:53 -0700

    Fix the styling.

--- js/script.js
+++ js/script.js
@@ -11,18 +11,47 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+    String.prototype.properCapitalize = function(){
+      return this.charAt(0).toUpperCase() + this.slice(1); 
+    };
+
     // load streetview
+    var street = $("#street").val() ;
+    if(street){
+        street = street.properCapitalize() + ",";
+    }
+    var city = $("#city").val().properCapitalize();
+    var address = street + city; 
 
-    // YOUR CODE GOES HERE!
-    var street = $("#street").val();
-    var city = $("#city").val();
-    var address = street + ", " + city; 
+    var location = city;
 
-    $greeting.text("So you want to live at " + street + ', ' + city + "?");
-    $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '">');
+    //WHY DOESN"T THIS WORK?!?!?! have to push buttom twice to see correct greeting.   
+    $('#street-btn, #city-btn').click(function(e) {
+        e.preventDefault();
+        if(this.id == 'street-btn') {
+            // console.log('street');
+            // loadData.location = address;
+            $greeting.text("Information about: " + address);
+            // return location;
+            // console.log(location);
+        }
+        else if (this.id == 'city-btn'){
+            // alert('city');
+            $greeting.text("Information about: " + city);
+            // console.log('city');
+            // loadData.location = city;
+            // return location;
+            // console.log(location);
+        }   
+    });
+   
+  
+    // $greeting.text("Information about: " + street  + " " + city);
+    $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + location + '">');
 
     //NY times AJAX request
-    var nytUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + city + "&api-key=e76feb03ae17bd533a0343f3ae88e251:13:74249074";
+    
+    var nytUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + location + "&api-key=e76feb03ae17bd533a0343f3ae88e251:13:74249074";
     $.getJSON(nytUrl, function(data){
         var urls = [];
         $.each(data.response.docs, function(index){
@@ -42,7 +71,7 @@ function loadData() {
         $wikiElem.text("Wikipedia Articles Could Not Be Loaded");
     }, 4000);
 
-    var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + city + "&format=json&callback=wikiCallback";
+    var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + location + "&format=json&callback=wikiCallback";
     $.ajax( {
         url: wikiUrl,
         dataType: 'jsonp', 
@@ -53,13 +82,14 @@ function loadData() {
                 var urlElem = "<li><a href='" + urlStr + "'>" + articles[i] + "</a></li>";
                 $wikiElem.append(urlElem);
             }
+            console.log(articles);
             clearTimeout(timer);
         },
         //^^^*********why does clearTimeout need to be in a function to work?
         
     });
-
     return false;
+    
 };
 
 $('#form-container').submit(loadData);
