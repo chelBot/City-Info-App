b0e0bb773d7fb92ce776e2a65a596d709a2fc9cd
chels <cstauber77@gmail.com>
2016-02-24 19:10:14 -0700

    Add if no articles found condition.

--- js/script.js
+++ js/script.js
@@ -12,13 +12,13 @@ function loadData() {
     $nytElem.text("");
 
     String.prototype.properCapitalize = function(){
-      return this.charAt(0).toUpperCase() + this.slice(1); 
+      return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase(); 
     };
 
     // load streetview
     var street = $("#street").val() ;
     if(street){
-        street = street.properCapitalize() + ",";
+        street = street.properCapitalize() + ", ";
     }
     var city = $("#city").val().properCapitalize();
     var address = street + city; 
@@ -26,70 +26,84 @@ function loadData() {
     var location = city;
 
     //WHY DOESN"T THIS WORK?!?!?! have to push buttom twice to see correct greeting.   
-    $('#street-btn, #city-btn').click(function(e) {
-        e.preventDefault();
-        if(this.id == 'street-btn') {
-            // console.log('street');
-            // loadData.location = address;
-            $greeting.text("Information about: " + address);
-            // return location;
-            // console.log(location);
-        }
-        else if (this.id == 'city-btn'){
-            // alert('city');
-            $greeting.text("Information about: " + city);
-            // console.log('city');
-            // loadData.location = city;
-            // return location;
-            // console.log(location);
-        }   
-    });
+    // $('#street-btn, #city-btn').click(function(e) {
+    //     e.preventDefault();
+    //     if(this.id == 'street-btn') {
+    //         // console.log('street');
+    //         // loadData.location = address;
+    //         $greeting.text("Information about: " + address);
+    //         // return location;
+    //         // console.log(location);
+    //     }
+    //     else if (this.id == 'city-btn'){
+    //         // alert('city');
+    //         $greeting.text("Information about: " + city);
+    //         // console.log('city');
+    //         // loadData.location = city;
+    //         // return location;
+    //         // console.log(location);
+    //     }   
+    // });
    
   
-    // $greeting.text("Information about: " + street  + " " + city);
-    $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + location + '">');
+    $greeting.text("Information about: " + address);
+    $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '">');
 
+    
+    var nytHasLI = false;
+    var wikiHasLI = false;
     //NY times AJAX request
-    
-    var nytUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + location + "&api-key=e76feb03ae17bd533a0343f3ae88e251:13:74249074";
-    $.getJSON(nytUrl, function(data){
-        var urls = [];
-        $.each(data.response.docs, function(index){
-            var url = data.response.docs[index]["web_url"];
-            var headline = data.response.docs[index]["headline"]["main"];
-            var snippet = data.response.docs[index]["snippet"];
-            //urls.push("<li id='" + index + "'><a href='" + url + "'>"  + headline +  "</a></li>");
-           // url.push("<li class='url'>" + data.response.docs["web url"] + "</li>");
-           $nytElem.append("<li id='" + index + "'><a href='" + url + "'>"  + headline +  "</a><p>" + snippet + "</p></li>")
-
-        });
-    }).error(function(){
-        $nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
-    });
-
-    var timer = setTimeout(function(){
-        $wikiElem.text("Wikipedia Articles Could Not Be Loaded");
-    }, 4000);
-
-    var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + location + "&format=json&callback=wikiCallback";
-    $.ajax( {
-        url: wikiUrl,
-        dataType: 'jsonp', 
-        success: function(response) { 
-            var articles = response[1];
-            for(var i = 0; i < articles.length; i++){
-                var urlStr = "https://en.wikipedia.org/wiki/" + articles[i];
-                var urlElem = "<li><a href='" + urlStr + "'>" + articles[i] + "</a></li>";
-                $wikiElem.append(urlElem);
+    var nytUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + address + "&api-key=e76feb03ae17bd533a0343f3ae88e251:13:74249074";
+    function ajax1() {
+        return $.getJSON(nytUrl, function(data){
+            var urls = [];
+            $.each(data.response.docs, function(index){
+                var url = data.response.docs[index]["web_url"];
+                var headline = data.response.docs[index]["headline"]["main"];
+                var snippet = data.response.docs[index]["snippet"];
+                //urls.push("<li id='" + index + "'><a href='" + url + "'>"  + headline +  "</a></li>");
+               // url.push("<li class='url'>" + data.response.docs["web url"] + "</li>");
+               $nytElem.append("<li id='" + index + "'><a href='" + url + "'>"  + headline +  "</a><p>" + snippet + "</p></li>")
+               
+            });
+            if($nytElem.find("li").length > 0){
+                nytHasLI = true;
             }
-            console.log(articles);
-            clearTimeout(timer);
-        },
-        //^^^*********why does clearTimeout need to be in a function to work?
-        
+            console.log(urls);
+        }).error(function(){
+            $nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
+        });
+    };
+    //Wikipedia Ajax request
+    var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + address + "&format=json&callback=wikiCallback";
+    function ajax2() { 
+        return $.ajax( {
+            url: wikiUrl,
+            dataType: 'jsonp', 
+            success: function(response) { 
+                var articles = response[1];
+                for(var i = 0; i < articles.length; i++){
+                    var urlStr = "https://en.wikipedia.org/wiki/" + articles[i];
+                    var urlElem = "<li><a href='" + urlStr + "'>" + articles[i] + "</a></li>";
+                    $wikiElem.append(urlElem);
+                }
+                if($wikiElem.find("li").length > 0) {
+                    wikiHasLI = true;
+                }
+                console.log(articles);
+            }    
+            }).error(function(){
+                $wikiElem.text("Wikipedia Articles Could Not Be Loaded");
+            });
+    };
+
+    //when both ajax requests return: 
+    $.when(ajax1(), ajax2()).done(function(){
+        if(!(wikiHasLI && nytHasLI)){
+            $nytElem.text("No articles found. Try boardening your scope and searching for articles within your city of interest instead.");
+        }
     });
-    return false;
-    
+    return false;   
 };
 
 $('#form-container').submit(loadData);
