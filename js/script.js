2d069827b8510adc248edb2c94e3df0f8d676301
chels <cstauber77@gmail.com>
2016-03-02 15:29:03 -0700

    Clean code.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 
-function loadData() {
-
+function loadData(){
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
@@ -22,81 +21,49 @@ function loadData() {
     }
     var city = $("#city").val().properCapitalize();
     var address = street + city; 
-
-    var location = city;
-
-    //WHY DOESN"T THIS WORK?!?!?! have to push buttom twice to see correct greeting.   
-    // $('#street-btn, #city-btn').click(function(e) {
-    //     e.preventDefault();
-    //     if(this.id == 'street-btn') {
-    //         // console.log('street');
-    //         // loadData.location = address;
-    //         $greeting.text("Information about: " + address);
-    //         // return location;
-    //         // console.log(location);
-    //     }
-    //     else if (this.id == 'city-btn'){
-    //         // alert('city');
-    //         $greeting.text("Information about: " + city);
-    //         // console.log('city');
-    //         // loadData.location = city;
-    //         // return location;
-    //         // console.log(location);
-    //     }   
-    // });
-   
   
     $greeting.text("Information about: " + address);
     $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '">');
 
-    
-    var nytHasLI = false;
-    var wikiHasLI = false;
     //NY times AJAX request
     var nytUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + address + "&api-key=e76feb03ae17bd533a0343f3ae88e251:13:74249074";
-    function ajax1() {
+    function ajax1(){
         return $.getJSON(nytUrl, function(data){
             var urls = [];
             $.each(data.response.docs, function(index){
                 var url = data.response.docs[index]["web_url"];
                 var headline = data.response.docs[index]["headline"]["main"];
                 var snippet = data.response.docs[index]["snippet"];
-                //urls.push("<li id='" + index + "'><a href='" + url + "'>"  + headline +  "</a></li>");
-               // url.push("<li class='url'>" + data.response.docs["web url"] + "</li>");
-               $nytElem.append("<li id='" + index + "'><a href='" + url + "'>"  + headline +  "</a><p>" + snippet + "</p></li>")
-               
+               $nytElem.append("<li id='" + index + "'><a href='" + url + "'>"  + headline +  "</a><p>" + snippet + "</p></li>")   
             });
             if($nytElem.find("li").length > 0){
                 nytHasLI = true;
             }
-            console.log(urls);
         }).error(function(){
             $nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
         });
     };
     //Wikipedia Ajax request
     var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + address + "&format=json&callback=wikiCallback";
-    function ajax2() { 
-        return $.ajax( {
+    function ajax2(){ 
+        return $.ajax({
             url: wikiUrl,
             dataType: 'jsonp', 
-            success: function(response) { 
+            success: function(response){ 
                 var articles = response[1];
                 for(var i = 0; i < articles.length; i++){
                     var urlStr = "https://en.wikipedia.org/wiki/" + articles[i];
                     var urlElem = "<li><a href='" + urlStr + "'>" + articles[i] + "</a></li>";
                     $wikiElem.append(urlElem);
                 }
-                if($wikiElem.find("li").length > 0) {
+                if($wikiElem.find("li").length > 0){
                     wikiHasLI = true;
                 }
-                console.log(articles);
             }    
             }).error(function(){
                 $wikiElem.text("Wikipedia Articles Could Not Be Loaded");
             });
     };
-
     //when both ajax requests return: 
     $.when(ajax1(), ajax2()).done(function(){
         if(!(wikiHasLI && nytHasLI)){
@@ -105,6 +72,5 @@ function loadData() {
     });
     return false;   
 };
-
 $('#form-container').submit(loadData);
 
